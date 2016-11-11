42ee9b8f0f65db0786d1593a43c150bae8d624ad
Guru <swati.guru@capgemini.com>
2016-11-11 16:21:22 +0530

    driver change

--- src/test/test.java
+++ src/test/test.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;  
 public class test {  
  public static void main(String args[]){  
-  WebDriver driver=new InternetExplorerDriver();  
+  WebDriver driver=new ChromeDriver();  
   System.out.println("Hello Google...");  
   driver.get("http://google.com");    
  }  
